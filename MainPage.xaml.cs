221fc22ed4b41f65cf84500168af939ce3b425f4
duc2071990 <duc2071990@gmail.com>
2016-06-04 09:57:10 +0700

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,41 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.UI.Xaml.Controls;
+
+
 
 namespace BaiTest
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-            int result = Calculate();
+            // lay so vi tri so 42
+            int result = Calculate(42);
             tbNumber.Text = result.ToString();
         }
 
-        public int Calculate()
+        public int Calculate(int numberial)
         {
             try
             {
-                int n = 0;
+                int n = 1;
                 int needNumbers = 0;
-                for (int i = 10000; n < 42; i++)
+                for (int i = 10000; n <= numberial; i++)
                 {
                     string strNumber = i.ToString();
 
@@ -49,7 +37,7 @@ namespace BaiTest
                     {
                         n++;
                     }
-                    if (n == 41)
+                    if (n == numberial)
                     {
                         needNumbers = i;
                     }
